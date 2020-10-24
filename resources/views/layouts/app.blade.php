a44309d5dc703ae27a86867f47c84faee406868e
AdityaSyah <adityasyah.mail@gmail.com>
2020-10-24 15:09:56 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title') - Platform Sewa Menyewakan Buku</title>
+    <link rel="stylesheet" href="{{ asset('public/assets/dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/assets/css/fontawesome-all.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/assets/css/style.css') }}">
+    <script src="{{ asset('public/js/app.js') }}" defer></script>
+    @yield('css')
+    <style>
+        
+    </style>
+    <livewire:styles/>
+</head>
+<body>
+    <livewire:header></livewire:header>
+    <div class="container mt-5 mb-5">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <!-- flash message -->
+                @if (session()->has('message'))
+                    <div class="alert alert-success">
+                        {{ session('message') }}
+                    </div>
+                @endif
+                <!-- end flash message -->
+
+                <!-- component -->
+                @yield('content')
+                <!-- end component -->
+            </div>
+        </div>
+    </div>
+    <script src="{{ asset('public/assets/js/jquery.js') }}"></script>
+    <script src="{{ asset('public/assets/dist/js/bootstrap.min.js') }}"></script>
+    <livewire:scripts/>
+</body>
+</html>
---
+++ resources/views/livewire/header.blade.php
@@ -0,0 +1,30 @@
+
+<div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-white nav-custom">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
+            aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#">BOOKO</a>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul class="navbar-nav ml-4 mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Kategori <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+            <form class="form-inline ml-4 my-2 my-lg-0 cari mr-auto">
+                <input class="mr-sm-2 input-cari" type="search" placeholder="Search" aria-label="Search">
+                <button class="my-2 my-sm-0 btn-cari" type="submit"><i class="fa fa-search"></i></button>
+            </form>
+            <ul class="navbar-nav ml-4 mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Daftar </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
